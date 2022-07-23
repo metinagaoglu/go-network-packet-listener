93fd2976341686fb9d278519d4c8f2df08e31ed5
Metin Ağaoğlu <metnagaoglu@gmail.com>
2022-07-24 00:21:27 +0300

    Create network package and parse packet.

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/metinagaoglu/package-dist
+module github.com/metinagaoglu/go-network-packet-listener
 
 go 1.17
 
@@ -8,7 +8,10 @@ require (
 )
 
 require (
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/klauspost/compress v1.14.2 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pierrec/lz4/v4 v4.1.14 // indirect
-	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 )
--- main.go
+++ main.go
@@ -4,10 +4,16 @@ import(
 	"github.com/google/gopacket/pcap"
 	"github.com/google/gopacket"
 
-	"fmt"
+	"github.com/metinagaoglu/go-network-packet-listener/network"
+
+	"github.com/fatih/color"
+
+	"context"
 );
 
 func main() {
+
+	ctx := context.Background()
 
 	//TODO: interface selection will be added
 	handle, err := pcap.OpenLive("eth0", 1600, true, pcap.BlockForever);
@@ -22,12 +28,14 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("tcp listening on eth0 interface...");
+	color.Red("tcp listening on eth0 interface...")
 
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
 		//fmt.Printf("%T \n", packet)
-		fmt.Println(packet);
+		//fmt.Println(packet);
+		network.HandlePacket(ctx, packet);
 	}	  
 }
+
 
